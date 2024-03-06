eec0bba716a4c036135fa3cc8eb8ad8c2cc42c74
SamuelT19 <samitesfay15@gmail.com>
2024-03-06 18:30:20 +0300

    g

--- controller/question/postQuestion.js
+++ controller/question/postQuestion.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 const postQuestion = async (req, res) => {
   const questionid = uuidv4();
   console.log(questionid);
-  const { userid, title, description } = req.body;
+  const { userid,title, description } = req.body;
   try {
     await dbConnection.execute(
       "INSERT INTO questions (questionid, userid, title, description) VALUES (?, ?, ?,?)",
@@ -14,7 +14,7 @@ const postQuestion = async (req, res) => {
     return res.status(200).json({ msg: "questQuestion inserted successfully" });
   } catch (error) {
     console.error("Error inserting question:", error);
-    return res.status(500).json({ error: "Error fetching question info" });
+    throw error;
   }
 };
 
